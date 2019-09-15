f2944b24fc24412a9355b20625001106317114ac
John Garabagiu <ion.g.garabagiu@gmail.com>
2019-09-15 16:34:44 +0300

    Fixed blank spots

--- sketch.js
+++ sketch.js
@@ -5,101 +5,104 @@ function setup() {
 	size = 3
 	strokeS = 4
 	clr = [206, 172, 65, 255]
-	
-	createCanvas(screenSize,screenSize)
+
+	createCanvas(screenSize, screenSize)
 	m = offset
 	n = offset
 	background(18)
 
-	 	// put setup code here
+	// put setup code here
 }
-function lr(x,y) {
+function lr(x, y) {
 	strokeWeight(strokeS)
 	stroke(...clr)
-	line(x,y,x+offset,y)
+	line(x, y, x + offset, y)
 }
-function ld(x,y) {
+function ld(x, y) {
 	strokeWeight(strokeS)
 	stroke(...clr)
-	line(x,y,x,y+offset)
+	line(x, y, x, y + offset)
 }
 
-function patternCheck(a,b){
-	swO = [[0,1,1],[1,1,1],[1,1,0]]
-	swT = [[1,1,0],[1,1,1],[0,1,1]]
-	blank = [[0,0,0],[0,0,0],[0,0,0]]
+function patternCheck(a, b) {
+	swOne = [[0, 1, 1], [1, 1, 1], [1, 1, 0]]
+	swTwo = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
 	if (
-		(mEqual(a, swO) && mEqual(b, swT)) || 
-		(mEqual(a, swT) && mEqual(b, swO)) || 
-		(mEqual(a, blank) && mEqual(b, blank))
+		(mEqual(a, swOne) && mEqual(b, swTwo)) ||
+		(mEqual(a, swTwo) && mEqual(b, swOne))
 	) {
 		a = scramble()
 		b = scramble()
-		patternCheck(a,b)
+		patternCheck(a, b)
 	}
-	return [a,b]
+	return [a, b]
 }
 function scramble() {
 	a = []
-	for (i = 0; i < size; i++){
+	for (i = 0; i < size; i++) {
 		b = []
-		for(j = 0; j < size; j++){
-			b.push(Math.random()<=percentage ? 1 : 0)
+		for (j = 0; j < size; j++) {
+			b.push(Math.random() <= percentage ? 1 : 0)
 		}
 		a.push(b)
 	}
 	return a
 }
-function printM(arr){
-	for (i=0; i<size; i++) {
+function printM(arr) {
+	for (i = 0; i < size; i++) {
 		console.log(a[i])
 	}
 }
-function mEqual(a,b){
-	for (i=0; i<size; i++){
-		for(j=0;j<size;j++){
-			if (a[i][j]!=b[i][j]){
+function mEqual(a, b) {
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (a[i][j] != b[i][j]) {
 				return false
 			}
 		}
 	}
 	return true
 }
-function glyph(x,y){
+function glyph(x, y) {
 
 	vs = scramble()
 	hs = scramble()
-	
+
 	if (size == 3) {
-		check = patternCheck(vs,hs)
+		check = patternCheck(vs, hs)
 		vs = check[0]
 		hs = check[1]
 	}
-	
-	for (i = 0; i<size; i++) {
-		for (j = 0; j<size; j++) {
-			if(j!=2){
-				if(hs[i][j] && hs[i][j+1]){
-					lr(x+(offset*j),y+(offset*i))
+	lines = 0
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (j != 2) {
+				if (hs[i][j] && hs[i][j + 1]) {
+					lr(x + (offset * j), y + (offset * i))
+					lines ++
 				}
 			}
-			
-			if(i!=2){
-				if(vs[i][j] && vs[i+1][j]){
-					ld(x+(offset*j),y+(offset*i))					
+
+			if (i != 2) {
+				if (vs[i][j] && vs[i + 1][j]) {
+					ld(x + (offset * j), y + (offset * i))
+					lines ++
 				}
 			}
 		}
 	}
+	if (lines == 0) {
+		glyph(x,y)
+	}
 	//noLoop()
 }
 function draw() {
-	glyph(m,n)
+	glyph(m, n)
 	if (m < screenSize) {
-		m += offset*(size+1)
+		m += offset * (size + 1)
 	} else if (n < screenSize) {
 		m = offset
-		n += offset*(size+1)
+		n += offset * (size + 1)
 		//noLoop()
 	} else {
 		m = offset
